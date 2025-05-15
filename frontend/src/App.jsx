e9c31d5c54df60d7c1637cc49f98306bad6b4b40
muhammadahmadhammad-dot <muhammadahmad6543217@gmail.com>
2025-05-15 14:18:48 +0500

    protected route for dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Pages/Layouts/Layout";
 import { Home } from "./Pages";
 import DashboardLayout from "./Pages/Layouts/DashboardLayout";
-import { Dashboard,AuthLogin } from "./Pages/Dashboard";
+import { Dashboard, AuthLogin } from "./Pages/Dashboard";
+import ProtectAdminPage from "./Pages/Dashboard/ProtectAdminPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        
         <Route path="" element={<Layout />}>
           <Route path="/" element={<Home />} />
         </Route>
@@ -16,9 +16,15 @@ function App() {
         {/* Admin Routes */}
         <Route path="/dashboard/login" element={<AuthLogin />} />
         <Route path="" element={<DashboardLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectAdminPage>
+                <Dashboard />
+              </ProtectAdminPage>
+            }
+          />
         </Route>
-
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/Pages/Dashboard/ProtectAdminPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {useSelector} from "react-redux"
+import { Navigate } from 'react-router-dom'
+
+const ProtectAdminPage = ({children}) => {
+    const isLoginedIn = useSelector((state)=>state.user.user ? true : false)
+    const isAdmin = useSelector((state)=>state.user.user.role == true ? true : false)
+
+     if (isLoginedIn && isAdmin) {
+      return <>{children}</>
+      ;
+    }
+
+  return (
+   <Navigate to="/dashboard/login" replace />
+  )
+}
+
+export default ProtectAdminPage
