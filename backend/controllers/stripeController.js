ec6945ae6878995b63103e72044f7318395b3ac8
muhammadahmadhammad-dot <muhammadahmad6543217@gmail.com>
2025-05-23 12:59:47 +0500

    backend stripe setup

---
+++ backend/controllers/stripeController.js
@@ -0,0 +1,17 @@
+import Stripe from "stripe";
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+export const payment = async (req, res) => {
+  const { amount } = req.body;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount, // amount in cents (e.g. $10 = 1000)
+      currency: "usd",
+    });
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { create,all,order,changeStatus } from "../controllers/orderController.js";
 import authMiddleware from "../middlewares/authmiddleware.js";
 import isAdminMiddleware from "../middlewares/isAdminMiddleware.js";
+import { payment } from "../controllers/stripeController.js";
 
 const orderRouter = Router();
 
 orderRouter.post("/create", create); //checkout
+orderRouter.post('/payment/create-payment-intent',payment); // stripe payment
 orderRouter.get("/",[authMiddleware, isAdminMiddleware], all);
 orderRouter.get("/:id",[authMiddleware, isAdminMiddleware], order);
 orderRouter.put("/update-status/:id",[authMiddleware, isAdminMiddleware], changeStatus);
