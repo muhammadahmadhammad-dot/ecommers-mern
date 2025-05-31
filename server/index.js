e0e0ccbc70a556fdbd3505693f5c26c4328f1046
muhammadahmadhammad-dot <muhammadahmad6543217@gmail.com>
2025-05-31 19:11:06 +0500

    change in server/index.js in cors

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ import cors from "cors";
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+  origin:["https://ecommers-mern-brown.vercel.app"],
+  credentials:true
+}));
 app.use(express.json()); //req.body
 
 connection(process.env.DB_URL);
