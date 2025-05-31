51008f9bd21af1bedf1cfd28a2b0518dd901591a
muhammadahmadhammad-dot <muhammadahmad6543217@gmail.com>
2025-05-31 20:03:02 +0500

    Changes for vercel

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import connection from "./config/dbConnection.js";
+import connection from "../config/dbConnection.js";
+import serverless from 'serverless-http';
 import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
@@ -17,10 +18,10 @@ app.use(express.json()); //req.body
 connection(process.env.DB_URL);
 
 
-import userRouter from "./routes/userRoutes.js";
-import categoryRouter from "./routes/categoryRouter.js";
-import productRouter from "./routes/productRouter.js";
-import orderRouter from "./routes/orderRouter.js";
+import userRouter from "../routes/userRoutes.js";
+import categoryRouter from "../routes/categoryRouter.js";
+import productRouter from "../routes/productRouter.js";
+import orderRouter from "../routes/orderRouter.js";
 
 // http://localhost:8000/api/users/
 app.use("/api/users", userRouter);
@@ -34,7 +35,8 @@ app.use("/api/products", productRouter);
 // http://localhost:8000/api/orders/
 app.use("/api/orders", orderRouter);
 
-const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log("Port is ready");
-});
+// const port = process.env.PORT || 8000;
+// app.listen(port, () => {
+//   console.log("Port is ready");
+// });
+export default serverless(app);
