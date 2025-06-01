beb3c815de545f85c2166c3239cb22c1537e3825
muhammadahmadhammad-dot <muhammadahmad6543217@gmail.com>
2025-06-01 11:53:36 +0500

    category btn setting

--- frontend/src/Components/Category.jsx
+++ frontend/src/Components/Category.jsx
@@ -24,7 +24,7 @@ const Category = ({
     fetchCategories();
   }, []);
   return (
-    <div className="grid grid-cols-1">
+    <div className="grid grid-cols-12 w-11/12 mx-auto">
       <button
         onClick={() => setSelectedCategoryId(null)}
         className={`btn btn-${
